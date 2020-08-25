933406ac2a3e193f6dc9235a6a8973ddeb7f85b6
Stefano Verna <s.verna@datocms.com>
2020-08-25 14:24:49 +0200

    Do not show third-party components if there are no news

--- src/components/ThirdPartyComponents.js
+++ src/components/ThirdPartyComponents.js
@@ -3,27 +3,29 @@ import format from 'date-fns/format';
 import parseIso from 'date-fns/parseISO';
 
 export default ({ feeds }) => {
-  console.log(feeds);
+  if (!feeds || feeds.length === 0) {
+    return null;
+  }
+
   return (
     <div className="incidents-daily">
       <div className="incidents-daily__title">Third-Party Components</div>
       <div>
-        {feeds &&
-          feeds.map(item => (
-            <div className={`incidents-daily__incident`} key={item.url}>
-              <h6 className="incidents-daily__incident__title">
-                <a href={item.url}>
-                  {item.source.name}: {item.title}
-                </a>
-              </h6>
-              <div className="incidents-daily__incident__update__description">
-                <div dangerouslySetInnerHTML={{ __html: item.description }} />
-              </div>
-              <p className="incidents-daily__incident__update__timestamp">
-                {format(parseIso(item.date), 'MMMM d, yyyy')}
-              </p>
+        {feeds.map(item => (
+          <div className={`incidents-daily__incident`} key={item.url}>
+            <h6 className="incidents-daily__incident__title">
+              <a href={item.url}>
+                {item.source.name}: {item.title}
+              </a>
+            </h6>
+            <div className="incidents-daily__incident__update__description">
+              <div dangerouslySetInnerHTML={{ __html: item.description }} />
             </div>
-          ))}
+            <p className="incidents-daily__incident__update__timestamp">
+              {format(parseIso(item.date), 'MMMM d, yyyy')}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
