252405770e2ecaccd25e3ab74f06f07b538d0c35
Stefano Verna <s.verna@datocms.com>
2020-08-25 13:34:37 +0200

    Small UI fix

--- src/components/ThirdPartyComponents.js
+++ src/components/ThirdPartyComponents.js
@@ -7,23 +7,24 @@ export default ({ feeds }) => {
   return (
     <div className="incidents-daily">
       <div className="incidents-daily__title">Third-Party Components</div>
-      {feeds &&
-        feeds.map(item => (
-          <div
-            className={`incidents-daily__incident`}
-            key={item.url}
-          >
-            <h6 className="incidents-daily__incident__title">
-              <a href={item.url}>{item.source.name}: {item.title}</a>
-            </h6>
-            <div className="incidents-daily__incident__update__description">
-              <div dangerouslySetInnerHTML={{ __html: item.description }} />
+      <div>
+        {feeds &&
+          feeds.map(item => (
+            <div className={`incidents-daily__incident`} key={item.url}>
+              <h6 className="incidents-daily__incident__title">
+                <a href={item.url}>
+                  {item.source.name}: {item.title}
+                </a>
+              </h6>
+              <div className="incidents-daily__incident__update__description">
+                <div dangerouslySetInnerHTML={{ __html: item.description }} />
+              </div>
+              <p className="incidents-daily__incident__update__timestamp">
+                {format(parseIso(item.date), 'MMMM d, yyyy')}
+              </p>
             </div>
-            <p className="incidents-daily__incident__update__timestamp">
-              {format(parseIso(item.date), 'MMMM d, yyyy')}
-            </p>
-          </div>
-        ))}
+          ))}
+      </div>
     </div>
   );
 };
